8ed9a6538fed45aa2d6ebcf210f44ae70385b9ab
RemiixInc <39117916+RemiixInc@users.noreply.github.com>
2021-01-07 12:02:01 +1100

    Update server.js

--- server.js
+++ server.js
@@ -17,9 +17,10 @@ app.use((req, res) => {
       var title = $('meta[property="og:title"]').attr('content') || $('title').text() || $('meta[name="title"]').attr('content')
       var description = $('meta[property="og:description"]').attr('content') || $('meta[name="description"]').attr('content')
       var url = $('meta[property="og:url"]').attr('content')
+      var site_name = $('meta[property="og:site_name"]').attr('content')
       var image = $('meta[property="og:image"]').attr('content') || $('meta[property="og:image:url"]').attr('content')
       var icon = $('link[rel="icon"]').attr('href') || $('link[rel="shortcut icon"]').attr('href')
-      res.json({ title: title, description: description, url: url, image: image, icon: icon })
+      res.json({ title: title, description: description, url: url, site_name: site_name, image: image, icon: icon })
     }).catch(err => {
       res.json({
         "error": "Invalid url query.",
